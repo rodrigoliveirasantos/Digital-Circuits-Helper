bb9074a27f30c623d10525bf713f95b665a9ef78
Rodrigo Santos Oliveira <rodrigoliveirasantos11@gmail.com>
2024-10-20 23:59:09 -0300

    feat: Estilização do teclado virtual

--- apps/site/src/app/modules/virtual-keyboard/components/virtual-keyboard/virtual-keyboard.component.ts
+++ apps/site/src/app/modules/virtual-keyboard/components/virtual-keyboard/virtual-keyboard.component.ts
@@ -1,6 +1,20 @@
 import { Component, effect, inject, input, TemplateRef, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VirtualKeyboardService } from '../../services/virtual-keyboard/virtual-keyboard.service';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+
+const ANIMATION_TIMING = "250ms cubic-bezier(.39,.33,.15,1.33)";
+
+const ANIMATION_HIDDEN_STYLE = {
+  transform: 'translateY(25%)',
+  opacity: 0
+};
+
+const ANIMATION_SHOW_STYLE = {
+  transform: 'translateY(0)',
+  opacity: 1
+}
 
 
 @Component({
@@ -9,6 +23,24 @@ import { VirtualKeyboardService } from '../../services/virtual-keyboard/virtual-
   imports: [CommonModule],
   templateUrl: './virtual-keyboard.component.html',
   styleUrl: './virtual-keyboard.component.scss',
+  animations: [
+    trigger('enterLeave', [
+      transition(
+        ':enter', 
+        [
+          style(ANIMATION_HIDDEN_STYLE),
+          animate(ANIMATION_TIMING, style(ANIMATION_SHOW_STYLE))
+        ] 
+      ),
+      transition(
+        ":leave",
+        [
+          style(ANIMATION_SHOW_STYLE),
+          animate(ANIMATION_TIMING, style(ANIMATION_HIDDEN_STYLE)),
+        ] 
+      )
+    ])
+  ]
 })
 export class VirtualKeyboardComponent {
   private readonly _virtualKeyboardService = inject(VirtualKeyboardService);
@@ -26,5 +58,5 @@ export class VirtualKeyboardComponent {
       })
     });
   }
-  
+
 }
